29847562dfe4c884b9f26aa3ea9ea4c6ef64d6f6
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2025-02-25 18:57:03 -0300

    First Commit

---
+++ transtokio/src/main/java/com/developer/tokio/transtokio/TranstokioApplication.java
@@ -0,0 +1,13 @@
+package com.developer.tokio.transtokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranstokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranstokioApplication.class, args);
+	}
+
+}
